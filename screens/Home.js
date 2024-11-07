a9234aa438a77a1efcf7d8274873d80a1ec1c58b
joswalt9 <joswalt9@ivytech.edu>
2024-11-07 13:47:12 -0500

    Imported listControllers

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
 import styles from "../styles";
+import ListContainer from "../listControllers/ListContainer";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Home Content</Text>
+    // <View style={styles.container}>
+    //   <Text>Home Content</Text>
+    // </View>
+
+    <View style={styles.listContainer}>
+      <ListContainer />
     </View>
   );
 }
