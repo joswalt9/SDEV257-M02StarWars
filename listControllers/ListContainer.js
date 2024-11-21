c13f9de9cdbfab0a9307c848e29ca285054e3669
joswalt9 <joswalt9@ivytech.edu>
2024-11-21 13:08:04 -0500

    Update

--- listControllers/ListContainer.js
+++ listControllers/ListContainer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { View } from "react-native";
 import { fetchItems, fetchFilms } from "../api";
 import List from "./List";
 import Modal from "../modal/Modal";
