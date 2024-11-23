8b6fd60f324c53c4d947ce0960e4be09d2d29f6a
joswalt9 <joswalt9@ivytech.edu>
2024-11-23 18:40:04 -0500

    Navigation Container Fix

--- App.js
+++ App.js
@@ -13,24 +13,21 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      {Platform.OS === "ios" && (
+      {Platform.OS === "ios" ? (
         <Tab.Navigator>
           <Tab.Screen name="Home" component={Home} />
           <Tab.Screen name="Planets" component={Planets} />
           <Tab.Screen name="Films" component={Films} />
           <Tab.Screen name="Spaceships" component={Spaceships} />
         </Tab.Navigator>
+      ) : (
+        <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="Planets" component={Planets} />
+          <Drawer.Screen name="Films" component={Films} />
+          <Drawer.Screen name="Spaceships" component={Spaceships} />
+        </Drawer.Navigator>
       )}
-
-      {Platform.OS === "android" ||
-        (Platform.OS === "web" && (
-          <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Planets" component={Planets} />
-            <Drawer.Screen name="Films" component={Films} />
-            <Drawer.Screen name="Spaceships" component={Spaceships} />
-          </Drawer.Navigator>
-        ))}
     </NavigationContainer>
   );
 }
